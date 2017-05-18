44f4aa8317eee4806f905309e26a66c549b16b6c
Klaus Hott Vidal <klahott@gmail.com>
2017-05-18 18:01:26 +1200

    Removing unused example Metacoin Contract

--- migrations/3_deploy_token_contracts.js
+++
@@ -1,7 +0,0 @@
-// var ERC20 = artifacts.require("./ERC20.sol");
-// var Token = artifacts.require("./Token.sol");
-var Gambit = artifacts.require("./Gambit.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Gambit);
-};
