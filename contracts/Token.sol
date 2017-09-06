668698bdcff8bfbdc6b316fa65a0855e55902a49
Andres Gonzalez <andres@3months.com>
2017-09-06 16:22:52 +1200

    solidity bump, refactor

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,32 +1,32 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.15;
 
 import "./ERC20.sol";
 
 contract Token is ERC20 {
   function () {
     // if ether is sent to this address, send it back.
-    throw;
+    require(false);
   }
 
   // Balances for each account
-  mapping(address => uint) balances;
+  mapping(address => uint256) balances;
 
   // Owner of account approves the transfer of an amount to another account
-  mapping(address => mapping (address => uint)) allowed;
+  mapping(address => mapping (address => uint256)) allowed;
 
   // The total token supply
-  uint internal _totalSupply;
+  uint256 internal _totalSupply;
 
   // Get the total token supply
   /// @return Total amount of tokens
-  function totalSupply() constant returns (uint totalSupply) {
+  function totalSupply() constant returns (uint256) {
     return _totalSupply;
   }
 
   // Get the account balance of another account with address _owner
   /// @param _owner The address from which the balance will be retrieved
   /// @return The balance
-  function balanceOf(address _owner) constant returns (uint balance) {
+  function balanceOf(address _owner) constant returns (uint256) {
     return balances[_owner];
   }
 
@@ -35,17 +35,15 @@ contract Token is ERC20 {
   /// @param _to The address of the recipient
   /// @param _value The amount of token to be transferred
   /// @return Whether the transfer was successful or not
-  function transfer(address _to, uint _value) returns (bool success) {
-    if (balances[msg.sender] >= _value
-        && _value > 0
-        && balances[_to] + _value > balances[_to]) {
-      balances[msg.sender] -= _value;
-      balances[_to]        += _value;
-      Transfer(msg.sender, _to, _value);
-      return true;
-    } else {
-      return false;
-    }
+  function transfer(address _to, uint256 _value) returns (bool) {
+    require(balances[msg.sender] >= _value);
+    require(_value > 0);
+    require(balances[_to] + _value > balances[_to]);
+
+    balances[msg.sender] -= _value;
+    balances[_to]        += _value;
+    Transfer(msg.sender, _to, _value);
+    return true;
   }
 
   // Send _value amount of tokens from address _from to address
@@ -54,19 +52,17 @@ contract Token is ERC20 {
   /// @param _to The address of the recipient
   /// @param _value The amount of token to be transferred
   /// @return Whether the transfer was successful or not
-  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-    if (balances[_from] >= _value
-         && allowed[_from][msg.sender] >= _value
-         && _value > 0
-         && balances[_to] + _value > balances[_to]) {
-      balances[_from] -= _value;
-      balances[_to]   += _value;
-      allowed[_from][msg.sender] -= _value;
-      Transfer(_from, _to, _value);
-      return true;
-    } else {
-      return false;
-    }
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+    require(balances[_from] >= _value);
+    require(_value > 0);
+    require(allowed[_from][msg.sender] >= _value);
+    require(balances[_to] + _value > balances[_to]);
+
+    balances[_from] -= _value;
+    balances[_to]   += _value;
+    allowed[_from][msg.sender] -= _value;
+    Transfer(_from, _to, _value);
+    return true;
   }
 
   // Allow _spender to withdraw from your account, multiple times, up to the
@@ -76,7 +72,7 @@ contract Token is ERC20 {
   /// @param _spender The address of the account able to transfer the tokens
   /// @param _value The amount of tokens to be approved for transfer
   /// @return Whether the approval was successful or not
-  function approve(address _spender, uint _value) returns (bool success) {
+  function approve(address _spender, uint256 _value) returns (bool) {
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
@@ -86,7 +82,7 @@ contract Token is ERC20 {
   /// @param _owner The address of the account owning tokens
   /// @param _spender The address of the account able to transfer the tokens
   /// @return Amount of remaining tokens allowed to spent
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
+  function allowance(address _owner, address _spender) constant returns (uint256) {
     return allowed[_owner][_spender];
   }
 }
