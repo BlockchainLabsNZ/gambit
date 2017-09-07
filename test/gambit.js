1dc26b938bb030646a22045403d62b86bf1b3c53
Andres Gonzalez <andres@3months.com>
2017-09-08 11:27:34 +1200

    fixed tests

--- test/gambit.js
+++ test/gambit.js
@@ -1,93 +1,46 @@
-var Gambit = artifacts.require("./Gambit.sol");
+const assertFail = require("./helpers/assertFail");
+const Gambit = artifacts.require("./Gambit.sol");
 
 contract("Gambit", function(accounts) {
   // CREATION
-  it("creation: test correct setting of vanity information", function(done) {
-    var ctr;
-    Gambit.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.name.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result, "Gambit");
-        return ctr.decimals.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 8);
-        return ctr.symbol.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result, "GAM");
-        done();
-      })
-      .catch(done);
+  it("creation: test correct setting of vanity information", async () => {
+    let ctr = await Gambit.new({ from: accounts[0] });
+    assert.strictEqual(await ctr.name.call(), "Gambit");
+    assert.strictEqual((await ctr.decimals.call()).toNumber(), 8);
+    assert.strictEqual(await ctr.symbol.call(), "GAM");
   });
 
   // BURNING
-  it("burning: owner of the contract is able to burn tokens", function(done) {
-    var ctr;
-    Gambit.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.totalBurnt.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 0);
-        return ctr.burn(100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        var logs = result.logs;
-        assert.equal(logs[0].event, "Burn");
-        assert.equal(logs[0].args._from, accounts[0]);
-        assert.strictEqual(logs[0].args._value.toNumber(), 100);
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 259999999999900);
-        return ctr.totalSupply.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 259999999999900);
-        return ctr.totalBurnt.call();
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 100);
-        done();
-      })
-      .catch(done);
+  it("burning: owner of the contract is able to burn tokens", async () => {
+    let ctr = await Gambit.new({ from: accounts[0] });
+    assert.strictEqual((await ctr.totalBurnt.call()).toNumber(), 0);
+    let logs = (await ctr.burn(100, { from: accounts[0] })).logs;
+    assert.equal(logs[0].event, "Burn");
+    assert.equal(logs[0].args._from, accounts[0]);
+    assert.strictEqual(logs[0].args._value.toNumber(), 100);
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[0])).toNumber(),
+      259999999999900
+    );
+    assert.strictEqual(
+      (await ctr.totalSupply.call()).toNumber(),
+      259999999999900
+    );
+    assert.strictEqual((await ctr.totalBurnt.call()).toNumber(), 100);
   });
 
-  it("burning: owner can only burn it's own tokens.", function(done) {
-    var ctr;
-    Gambit.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.transfer(accounts[1], 250000000000000, {
-          from: accounts[0]
-        });
-      })
-      .then(function(result) {
-        return ctr.burn.call(50000000000000, { from: accounts[0] });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+  it("burning: owner can only burn it's own tokens.", async () => {
+    let ctr = await Gambit.new({ from: accounts[0] });
+    await ctr.transfer(accounts[1], 250000000000000, {
+      from: accounts[0]
+    });
+    await assertFail(
+      async () => await ctr.burn.call(50000000000000, { from: accounts[0] })
+    );
   });
 
-  it("burning: owner can only burn it's own tokens.", function(done) {
-    var ctr;
-    Gambit.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.burn.call(0, { from: accounts[0] });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+  it("burning: owner can only burn it's own tokens.", async () => {
+    let ctr = await Gambit.new({ from: accounts[0] });
+    await assertFail(async () => await ctr.burn.call(0, { from: accounts[0] }));
   });
 });
--- test/token.js
+++ test/token.js
@@ -1,269 +1,153 @@
-var Token = artifacts.require("./Gambit.sol");
+const assertFail = require("./helpers/assertFail");
+const Token = artifacts.require("./Gambit.sol");
 
 contract("Token", function(accounts) {
   // CREATION
-  it("creation: should create an initial balance of 260000000000000 for the creator", function(
-    done
-  ) {
-    Token.new({ from: accounts[0] })
-      .then(function(ctr) {
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 260000000000000);
-        done();
-      })
-      .catch(done);
+  it("creation: should create an initial balance of 260000000000000 for the creator", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[0])).toNumber(),
+      260000000000000
+    );
   });
 
   // TRANSERS
-  it("transfers: should transfer 260000000000000 to accounts[1] with accounts[0] having 1000000", function(
-    done
-  ) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.transfer(accounts[1], 260000000000000, {
-          from: accounts[0]
-        });
-      })
-      .then(function(result) {
-        var logs = result.logs;
-        assert.equal(logs[0].event, "Transfer");
-        assert.equal(logs[0].args._from, accounts[0]);
-        assert.equal(logs[0].args._to, accounts[1]);
-        assert.strictEqual(logs[0].args._value.toNumber(), 260000000000000);
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 0);
-        return ctr.balanceOf.call(accounts[1]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 260000000000000);
-        done();
-      })
-      .catch(done);
+  it("transfers: should transfer 260000000000000 to accounts[1] with accounts[0] having 1000000", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    let logs = (await ctr.transfer(accounts[1], 260000000000000, {
+      from: accounts[0]
+    })).logs;
+
+    assert.equal(logs[0].event, "Transfer");
+    assert.equal(logs[0].args._from, accounts[0]);
+    assert.equal(logs[0].args._to, accounts[1]);
+    assert.strictEqual(logs[0].args._value.toNumber(), 260000000000000);
+    assert.strictEqual((await ctr.balanceOf.call(accounts[0])).toNumber(), 0);
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[1])).toNumber(),
+      260000000000000
+    );
   });
 
-  it("transfers: should fail when trying to transfer 260000000000001 to accounts[1] with accounts[0] having 1000000", function(
-    done
-  ) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.transfer.call(accounts[1], 260000000000001, {
+  it("transfers: should fail when trying to transfer 260000000000001 to accounts[1] with accounts[0] having 1000000", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await assertFail(
+      async () =>
+        await ctr.transfer.call(accounts[1], 260000000000001, {
           from: accounts[0]
-        });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+        })
+    );
   });
 
-  it("transfers: should fail when trying to transfer zero.", function(done) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.transfer.call(accounts[1], 0, { from: accounts[0] });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+  it("transfers: should fail when trying to transfer zero.", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await assertFail(
+      async () => await ctr.transfer.call(accounts[1], 0, { from: accounts[0] })
+    );
   });
 
   // APPROVALS
-  it("approvals: msg.sender should approve 100 to accounts[1]", function(done) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.approve(accounts[1], 100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        var logs = result.logs;
-        assert.equal(logs[0].event, "Approval");
-        assert.equal(logs[0].args._owner, accounts[0]);
-        assert.equal(logs[0].args._spender, accounts[1]);
-        assert.strictEqual(logs[0].args._value.toNumber(), 100);
-        return ctr.allowance.call(accounts[0], accounts[1]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 100);
-        done();
-      })
-      .catch(done);
+  it("approvals: msg.sender should approve 100 to accounts[1]", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    let logs = (await ctr.approve(accounts[1], 100, { from: accounts[0] }))
+      .logs;
+    assert.equal(logs[0].event, "Approval");
+    assert.equal(logs[0].args._owner, accounts[0]);
+    assert.equal(logs[0].args._spender, accounts[1]);
+    assert.strictEqual(logs[0].args._value.toNumber(), 100);
+
+    assert.strictEqual(
+      (await ctr.allowance.call(accounts[0], accounts[1])).toNumber(),
+      100
+    );
   });
 
-  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 20 once.", function(
-    done
-  ) {
-    var ctr, watcher;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        watcher = ctr.Transfer();
-        return ctr.approve(accounts[1], 100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        return ctr.balanceOf.call(accounts[2]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 0);
-        return ctr.transferFrom(accounts[0], accounts[2], 20, {
-          from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        var logs = result.logs;
-        assert.equal(logs[0].event, "Transfer");
-        assert.equal(logs[0].args._from, accounts[0]);
-        assert.equal(logs[0].args._to, accounts[2]);
-        assert.strictEqual(logs[0].args._value.toNumber(), 20);
-        return ctr.allowance.call(accounts[0], accounts[1]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 80);
-        return ctr.balanceOf.call(accounts[2]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 20);
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 259999999999980);
-        done();
-      })
-      .catch(done);
+  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 20 once.", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await ctr.approve(accounts[1], 100, { from: accounts[0] });
+    assert.strictEqual((await ctr.balanceOf.call(accounts[2])).toNumber(), 0);
+    let logs = (await ctr.transferFrom(accounts[0], accounts[2], 20, {
+      from: accounts[1]
+    })).logs;
+
+    assert.equal(logs[0].event, "Transfer");
+    assert.equal(logs[0].args._from, accounts[0]);
+    assert.equal(logs[0].args._to, accounts[2]);
+    assert.strictEqual(logs[0].args._value.toNumber(), 20);
+    assert.strictEqual(
+      (await ctr.allowance.call(accounts[0], accounts[1])).toNumber(),
+      80
+    );
+    assert.strictEqual((await ctr.balanceOf.call(accounts[2])).toNumber(), 20);
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[0])).toNumber(),
+      259999999999980
+    );
   });
 
-  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 20 twice.", function(
-    done
-  ) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.approve(accounts[1], 100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        return ctr.transferFrom(accounts[0], accounts[2], 20, {
-          from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        return ctr.transferFrom(accounts[0], accounts[2], 20, {
-          from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        return ctr.allowance.call(accounts[0], accounts[1]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 60);
-        return ctr.balanceOf.call(accounts[2]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 40);
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 259999999999960);
-        done();
-      })
-      .catch(done);
+  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 20 twice.", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await ctr.approve(accounts[1], 100, { from: accounts[0] });
+    await ctr.transferFrom(accounts[0], accounts[2], 20, { from: accounts[1] });
+    await ctr.transferFrom(accounts[0], accounts[2], 20, { from: accounts[1] });
+    assert.strictEqual(
+      (await ctr.allowance.call(accounts[0], accounts[1])).toNumber(),
+      60
+    );
+    assert.strictEqual((await ctr.balanceOf.call(accounts[2])).toNumber(), 40);
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[0])).toNumber(),
+      259999999999960
+    );
   });
 
   //should approve 100 of msg.sender & withdraw 50 & 60 (should fail).
-  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 50 & 60 (2nd tx should fail)", function(
-    done
-  ) {
-    var ctr;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.approve(accounts[1], 100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        return ctr.transferFrom(accounts[0], accounts[2], 50, {
+  it("approvals: msg.sender approves accounts[1] of 100 & withdraws 50 & 60 (2nd tx should fail)", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await ctr.approve(accounts[1], 100, { from: accounts[0] });
+    await ctr.transferFrom(accounts[0], accounts[2], 50, {
+      from: accounts[1]
+    });
+    assert.strictEqual(
+      (await ctr.allowance.call(accounts[0], accounts[1])).toNumber(),
+      50
+    );
+    assert.strictEqual((await ctr.balanceOf.call(accounts[2])).toNumber(), 50);
+    assert.strictEqual(
+      (await ctr.balanceOf.call(accounts[0])).toNumber(),
+      259999999999950
+    );
+
+    await assertFail(
+      async () =>
+        await ctr.transferFrom.call(accounts[0], accounts[2], 60, {
           from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        return ctr.allowance.call(accounts[0], accounts[1]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 50);
-        return ctr.balanceOf.call(accounts[2]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 50);
-        return ctr.balanceOf.call(accounts[0]);
-      })
-      .then(function(result) {
-        assert.strictEqual(result.toNumber(), 259999999999950);
-        return ctr.transferFrom.call(accounts[0], accounts[2], 60, {
-          from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+        })
+    );
   });
 
-  it("approvals: attempt withdrawal from acconut with no allowance (should fail)", function(
-    done
-  ) {
-    var ctr = null;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.transferFrom.call(accounts[0], accounts[2], 60, {
+  it("approvals: attempt withdrawal from acconut with no allowance (should fail)", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await assertFail(
+      async () =>
+        await ctr.transferFrom.call(accounts[0], accounts[2], 60, {
           from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+        })
+    );
   });
 
-  it("approvals: allow accounts[1] 100 to withdraw from accounts[0]. Withdraw 60 and then approve 0 & attempt transfer.", function(
-    done
-  ) {
-    var ctr = null;
-    Token.new({ from: accounts[0] })
-      .then(function(result) {
-        ctr = result;
-        return ctr.approve(accounts[1], 100, { from: accounts[0] });
-      })
-      .then(function(result) {
-        return ctr.transferFrom(accounts[0], accounts[2], 60, {
+  it("approvals: allow accounts[1] 100 to withdraw from accounts[0]. Withdraw 60 and then approve 0 & attempt transfer.", async () => {
+    let ctr = await Token.new({ from: accounts[0] });
+    await ctr.approve(accounts[1], 100, { from: accounts[0] });
+    await ctr.transferFrom(accounts[0], accounts[2], 60, {
+      from: accounts[1]
+    });
+    await ctr.approve(accounts[1], 0, { from: accounts[0] });
+    await assertFail(
+      async () =>
+        await ctr.transferFrom.call(accounts[0], accounts[2], 10, {
           from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        return ctr.approve(accounts[1], 0, { from: accounts[0] });
-      })
-      .then(function(result) {
-        return ctr.transferFrom.call(accounts[0], accounts[2], 10, {
-          from: accounts[1]
-        });
-      })
-      .then(function(result) {
-        assert.isFalse(result);
-        done();
-      })
-      .catch(done);
+        })
+    );
   });
 });
